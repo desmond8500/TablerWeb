7ffbb6ef5a9e6fe4832f384760a36eed1fe74344
kratos <desmond8500@gmail.com>
2022-11-10 01:15:23 +0000

    stock update

--- src/app/stock/stock.module.ts
+++ src/app/stock/stock.module.ts
@@ -7,6 +7,7 @@ import { StockComponent } from './stock.component';
 import { ArticlesComponent } from '../stock/articles/articles/articles.component';
 import { ArticleCardComponent } from '../stock/articles/article-card/article-card.component';
 import { ArticleAddComponent } from '../stock/articles/article-add/article-add.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { ArticleAddComponent } from '../stock/articles/article-add/article-add.c
   ],
   imports: [
     CommonModule,
-    StockRoutingModule
+    StockRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class StockModule { }
