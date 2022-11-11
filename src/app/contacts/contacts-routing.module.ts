20a40d591bcef761b102923fa8b9fb1de0618076
kratos <desmond8500@gmail.com>
2022-11-11 19:23:32 +0000

    test

--- src/app/contacts/contacts-routing.module.ts
+++ src/app/contacts/contacts-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from '../pages/not-found/not-found.component';
-// import { ContactsComponent } from './contacts.component';
+import { ContactsComponent } from './contacts.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'contacts', pathMatch: 'full'},
-  // { path: 'contacts', component: ContactsComponent },
+  { path: 'contacts', component: ContactsComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
--- src/app/contacts/contacts.module.ts
+++ src/app/contacts/contacts.module.ts
@@ -4,14 +4,16 @@ import { CommonModule } from '@angular/common';
 import { ContactsRoutingModule } from './contacts-routing.module';
 import { ContactAddComponent } from './contact-add/contact-add.component';
 import { ContactCardComponent } from './contact-card/contact-card.component';
-// import { ContactsComponent } from './contacts.component';
+import { ContactsComponent } from './contacts.component';
+import { HeaderComponent } from '../index/header/header.component';
 
 
 @NgModule({
   declarations: [
     ContactAddComponent,
     ContactCardComponent,
-    // ContactsComponent
+    ContactsComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
