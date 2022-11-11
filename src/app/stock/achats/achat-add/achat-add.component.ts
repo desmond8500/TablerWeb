426b0eb2b166473072713e9211002517a05e2610
kratos <desmond8500@gmail.com>
2022-11-11 00:05:08 +0000

    achats fix

--- src/app/stock/achats/achat-add/achat-add.component.ts
+++ src/app/stock/achats/achat-add/achat-add.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AchatService } from 'src/app/services/achat.service';
 
 @Component({
   selector: 'app-achat-add',
@@ -7,9 +10,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AchatAddComponent implements OnInit {
 
-  constructor() { }
+  @Output() reloadEvent = new EventEmitter()
+    constructor(
+      private modalService: NgbModal,
+      private _achat: AchatService,
+      private fb: FormBuilder,
+      ) {}
 
-  ngOnInit(): void {
+    brandForm: FormGroup = this.fb.group({
+      name: new FormControl(null, [Validators.required]),
+      date: new FormControl(),
+      description: new FormControl(),
+    })
+
+    ngOnInit(): void {
+    }
+
+    addAchat(){
+      let form: any = this.brandForm.value
+      console.log(form);
+
+      this._achat.addAchat(form).subscribe({
+        next: (res) => {
+          console.log(res)
+          this.reloadEvent.emit()
+          this.modalService.dismissAll()
+        },
+        error: (error) => console.log(error),
+      })
+    }
+
+
+  closeResult = '';
+
+  open(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: result`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
   }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: reason`;
+    }
+  }
+
 
 }
